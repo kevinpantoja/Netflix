c453f02ecd2919b7f2319507a2f95e5c648af8e2
Kevin Pantoja <kevinpantoja146@gmail.com>
2022-02-06 21:31:55 -0500

    login y account terminado

--- js/login.js
+++ js/login.js
@@ -1,29 +1,36 @@
 const inputs = document.querySelectorAll(".formulario__textbox");
 let formulario = document.getElementById("formulario");
-
 let correo = "usuario@gmail.com";
 let password = "123456";
-
 const expresiones = {
     password: /^.{6,40}$/,
 	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
 }
-
 const campos = {
 	password: false,
 	correo: false,
 }
-
 const validarCampo = (expresion, input, campo) => {
 	if(expresion.test(input.value)){
 		document.getElementById(`notify__${campo}`).style.display = "none";
+		if(campo == "password")
+			inputs[1].style.borderBottom = "none";
+		else
+			inputs[0].style.borderBottom = "none";
 		campos[campo] = true;
 	} else {
 		document.getElementById(`notify__${campo}`).style.display = "block";
+		if(campo == "password"){
+			inputs[1].style.borderBottom = ".2rem solid #E87C03";
+			inputs[1].style.marginBottom = ".3rem";
+		}
+		else{
+			inputs[0].style.borderBottom = ".2rem solid #E87C03";
+			inputs[0].style.marginBottom = ".3rem";
+		}
 		campos[campo] = false;
 	}
 }
-
 const validarFormulario = (e) => {
 	switch (e.target.name) {
 		case "password":
@@ -34,13 +41,10 @@ const validarFormulario = (e) => {
 		break;
 	}
 }
-
 for(const input of inputs){
     input.addEventListener('keyup', validarFormulario);
 	input.addEventListener('blur', validarFormulario);
 }
-
-
 formulario.addEventListener('submit', (e) => {
     e.preventDefault();
     if(campos.correo && campos.password){
